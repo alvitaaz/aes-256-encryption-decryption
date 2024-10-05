f4a235367c12ebaae0b55ad6a557137be8cd4999
alvitaaz <alvita.azalia@student.president.ac.id>
2024-10-06 02:34:17 +0700

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,20 +1,19 @@
 <?php
-// Koneksi ke database
+// koneksi ke database
 $conn = new mysqli('localhost', 'root', '', 'secure_app_db');
 
-// Cek koneksi
+// cek koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Username dan password
 $username = 'alvita';
-$password = 'admin'; // Kata sandi yang ingin Anda gunakan
+$password = 'admin'; 
 
-// Hash kata sandi
+// hash kata sandi
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// Simpan pengguna dengan kata sandi yang telah di-hash
+// simpan pengguna dengan kata sandi yang telah di-hash
 $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ss", $username, $hashed_password);
